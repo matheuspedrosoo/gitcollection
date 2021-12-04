72278cb42c0229d70417903d7c431fac83a6aa2d
Matheus Pedroso <matheuspedroso404@gmail.com>
2021-12-04 17:33:36 -0300

    create validate

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,33 +1,76 @@
 import React from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
-import { Title, Form, Repos } from './styles';
+import { api } from '../../services/api';
+import { Title, Form, Repos, Error } from './styles';
 import logo from '../../assets/logo.svg';
 
+interface GithubRepository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 export const Dashboard: React.FC = () => {
+  const [repos, setRepos] = React.useState<GithubRepository[]>([]);
+  const [newRepo, setNewRepo] = React.useState('');
+  const [inputError, setInpuError] = React.useState('');
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    setNewRepo(event.target.value);
+  }
+
+  async function handleAddRepo(
+    event: React.FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInpuError('Informe o username/repositório');
+      return;
+    }
+
+    const response = await api.get<GithubRepository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepos([...repos, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logo} alt="GitCollection" />
       <Title>Catálogo de repositórios do Github</Title>
 
-      <Form>
-        <input placeholder="username/repository_name" />
+      <Form hasError={Boolean(inputError)} onSubmit={handleAddRepo}>
+        <input
+          placeholder="username/repository_name"
+          onChange={handleInputChange}
+        />
         <button type="submit">Buscar</button>
       </Form>
 
-      <Repos>
-        <a href="/repositories">
-          <img
-            src="https://avatars.githubusercontent.com/u/76970330?v=4"
-            alt="Repositorio"
-          />
-          <div>
-            <strong>Matheus/curso-react</strong>
-            <p>Repositório do mini-curso React</p>
-          </div>
+      {inputError && <Error> {inputError}</Error>}
 
-          <FiChevronRight size={20} />
-        </a>
+      <Repos>
+        {repos.map(repository => (
+          <a href="/repositories" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repos>
     </>
   );
